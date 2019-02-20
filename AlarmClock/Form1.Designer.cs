6fb1754e573f219903f7f6040ec731f4c9bc6956
Diana Arita <dianaarita500@gmail.com>
2019-02-20 07:20:44 -0600

    First commit

---
+++ AlarmClock/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace AlarmClock
+{
+    partial class Alarm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker.Location = new System.Drawing.Point(42, 40);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(297, 29);
+            this.dateTimePicker.TabIndex = 0;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(42, 138);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 48);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(213, 138);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(126, 48);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(163, 87);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(68, 25);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status";
+            // 
+            // Alarm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 198);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dateTimePicker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Alarm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Alarm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ AlarmClock/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Inspiration code: https://www.youtube.com/watch?time_continue=9&v=GJbVEZkeImk
+
+namespace AlarmClock
+{
+    public partial class Alarm : Form
+    {
+        System.Timers.Timer timer;
+
+        //delefate label threads are used to prevent the system from going into an inconsistent state
+        //it's not safe to call a control from a thread without using the invoke method
+        delegate void UpdateLable(Label lbl, string value);
+        void UpdateDataLable(Label lbl, string value)
+        {
+            lbl.Text = value;
+        }
+
+        public Alarm()
+        {
+            InitializeComponent();
+        }
+
+        private void Alarm_Load(object sender, EventArgs e)
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime currentTIme = DateTime.Now;
+            DateTime userTime = dateTimePicker.Value;
+            if(currentTIme.Hour == userTime.Hour && currentTIme.Minute == userTime.Minute && currentTIme.Second == userTime.Second)
+            {
+                timer.Stop();
+                try
+                {
+                    UpdateLable upd = UpdateDataLable;
+                    if(lblStatus.InvokeRequired)
+                    {
+                        Invoke(upd, lblStatus, "Stop");
+                    }
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = @"C:\Windows\media\Alarm01.wav";
+                    player.PlayLooping();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            lblStatus.Text = "Running...";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            lblStatus.Text = "Stop";
+        }
+        
+    }
+}
